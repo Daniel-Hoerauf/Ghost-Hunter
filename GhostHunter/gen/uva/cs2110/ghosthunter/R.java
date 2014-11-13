fe8912b34e2667520137f06f7173e86b58031dbb
Daniel-Hoerauf <dah4pe@virginia.edu>
2014-11-13 18:37:42 -0500

    Added a Ghost interface

--- GhostHunter/gen/uva/cs2110/ghosthunter/R.java
+++ GhostHunter/gen/uva/cs2110/ghosthunter/R.java
@@ -45,8 +45,9 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class raw {
-        public static final int splash_screen_chimes=0x7f040000;
-        public static final int splash_screen_gong=0x7f040001;
+        public static final int biosphere=0x7f040000;
+        public static final int splash_screen_chimes=0x7f040001;
+        public static final int splash_screen_gong=0x7f040002;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
