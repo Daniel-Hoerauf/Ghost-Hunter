967c4b2e0a67791f2a4ef7001b5e0226e76ab0c6
Daniel-Hoerauf <dah4pe@virginia.edu>
2014-12-02 17:31:53 -0500

    Game Resets upon leaving and removed options menu

--- GhostHunter/gen/uva/cs2110/ghosthunter/R.java
+++ GhostHunter/gen/uva/cs2110/ghosthunter/R.java
@@ -41,8 +41,8 @@ public final class R {
         public static final int action_settings=0x7f09000c;
         public static final int gameView1=0x7f090005;
         public static final int jumpButton=0x7f090004;
-        public static final int optionsButton=0x7f090001;
-        public static final int playButton=0x7f090002;
+        public static final int optionsButton=0x7f090002;
+        public static final int playButton=0x7f090001;
         public static final int shootButton=0x7f090003;
         public static final int welcome=0x7f090000;
     }
@@ -50,7 +50,6 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash=0x7f030001;
         public static final int game_main=0x7f030002;
-        public static final int options_menu=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
